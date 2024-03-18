a0354bdeb55012d24e65c2bfda88345c6bf42c6c
emil-kirov <ekirov6@gmail.com>
2024-03-18 14:53:48 -0700

    init files

---
+++ src/test/java/com/cointracker/cointrackertakehome/CointrackerTakehomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cointracker.cointrackertakehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CointrackerTakehomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
