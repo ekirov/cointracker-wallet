550dd5fade20ea4aca168c33fcd371c71390cb2f
emil-kirov <ekirov6@gmail.com>
2024-03-18 15:36:06 -0700

    init service with methods

---
+++ src/main/java/com/cointracker/cointrackertakehome/service/BitcoinWalletService.java
@@ -0,0 +1,11 @@
+package com.cointracker.cointrackertakehome.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface BitcoinWalletService {
+    void addAddress(String address);
+    void removeAddress(String address);
+    List<String> getTransactions(String address);
+    BigDecimal getBalance(String address);
+}
