4455b7c276bce2dabe7b381680040ff778d4f4fe
emil-kirov <ekirov6@gmail.com>
2024-03-18 15:36:06 -0700

    init service with methods

--- src/main/java/com/cointracker/cointrackertakehome/controller/BitcoinWalletController.java
+++ src/main/java/com/cointracker/cointrackertakehome/controller/BitcoinWalletController.java
@@ -1,29 +1,46 @@
 package com.cointracker.cointrackertakehome.controller;
 
+import com.cointracker.cointrackertakehome.service.BitcoinWalletService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Controller
 @RequestMapping("/api/bitcoin-wallet")
 public class BitcoinWalletController {
+
+    private final BitcoinWalletService bitcoinWalletService;
+
+    @Autowired
+    public BitcoinWalletController(BitcoinWalletService bitcoinWalletService){
+        this.bitcoinWalletService = bitcoinWalletService;
+    }
+
     @PostMapping("/address/add")
     public ResponseEntity<?> addAddress(@RequestBody String address){
+        bitcoinWalletService.addAddress(address);
         return ResponseEntity.ok().body("address added");
     }
 
     @PostMapping("/address/remove")
     public ResponseEntity<?> removeAddress(@RequestBody String address){
+        bitcoinWalletService.removeAddress(address);
         return ResponseEntity.ok().body("address removed");
     }
 
     @GetMapping("/address/transactions")
     public ResponseEntity<?> getTransactions(@PathVariable String address){
-        return ResponseEntity.ok().body("transaction data for address");
+        List<String> transactions = bitcoinWalletService.getTransactions(address);
+        return ResponseEntity.ok().body(transactions);
     }
 
     @GetMapping("/address/balance")
     public ResponseEntity<?> getBalance(@PathVariable String address){
-        return ResponseEntity.ok().body("balance for address");
+        BigDecimal balance = bitcoinWalletService.getBalance(address);
+        return ResponseEntity.ok().body(balance);
     }
 }
---
+++ src/main/java/com/cointracker/cointrackertakehome/service/impl/BitcoinWalletServiceImpl.java
@@ -0,0 +1,29 @@
+package com.cointracker.cointrackertakehome.service.impl;
+
+import com.cointracker.cointrackertakehome.service.BitcoinWalletService;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+@Service
+public class BitcoinWalletServiceImpl implements BitcoinWalletService {
+    @Override
+    public void addAddress(String address) {
+
+    }
+
+    @Override
+    public void removeAddress(String address) {
+
+    }
+
+    @Override
+    public List<String> getTransactions(String address) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getBalance(String address) {
+        return null;
+    }
+}
