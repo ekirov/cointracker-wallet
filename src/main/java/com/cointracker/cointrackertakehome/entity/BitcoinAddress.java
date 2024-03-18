b14afab1cedbb5a6b6d747d71deb935168f03508
emil-kirov <ekirov6@gmail.com>
2024-03-18 16:07:36 -0700

    init entity and repo and removal of security for testing

---
+++ src/main/java/com/cointracker/cointrackertakehome/entity/BitcoinAddress.java
@@ -0,0 +1,49 @@
+package com.cointracker.cointrackertakehome.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.sql.Timestamp;
+
+@Entity
+public class BitcoinAddress {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String address;
+
+    private Timestamp createdAt;
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "BitcoinAddress{" +
+                "id=" + id +
+                ", address='" + address + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
