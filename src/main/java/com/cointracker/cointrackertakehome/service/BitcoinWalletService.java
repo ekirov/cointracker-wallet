1a094232e5461bef35d3e92ba6d40f751e89c840
emil-kirov <ekirov6@gmail.com>
2024-03-18 19:01:04 -0700

    get transactions basic

--- src/main/java/com/cointracker/cointrackertakehome/service/BitcoinWalletService.java
+++ src/main/java/com/cointracker/cointrackertakehome/service/BitcoinWalletService.java
@@ -1,12 +1,14 @@
 package com.cointracker.cointrackertakehome.service;
 
+import com.cointracker.cointrackertakehome.entity.BitcoinTransaction;
+
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface BitcoinWalletService {
     void addAddress(String address);
     void removeAddress(String address);
-    List<String> getTransactions(String address);
+    List<BitcoinTransaction> getTransactions(String address);
     BigDecimal getBalance(String address);
 
     void synchronizeTransactions();
